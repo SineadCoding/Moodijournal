5c28e938d27cfecacef72189625bc325c59fd7f2
Sinead <sineaddejager@gmail.com>
2025-07-15 10:51:00 +0200

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 
-user_email = st.user.email
 st.title("MoodiJournal📓")
 st.write("Welcome to MoodiJournal, a safe space to write about your day or your feelings throughout the day and assess your mood from a nuetral perpective, "
                  " log your mood and content from your journal for you to reflect on and recieve a special message based off of your mood to either support or enhance you day.")
