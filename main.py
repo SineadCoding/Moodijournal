34a779c3f3d23aa4ecadb039790ff5a127de0398
Sinead <sineaddejager@gmail.com>
2025-07-21 10:03:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from textblob import TextBlob
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import random
+from pandas.errors import EmptyDataError
+from PIL import Image
+import os
 
 st.title("MoodiJournal📓")
 st.write("Welcome to MoodiJournal, a safe space to write about your day or your feelings throughout the day and assess your mood from a nuetral perpective, "
