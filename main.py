594ca86b45992b33429f48ee72c1202b66bdb219
Sinead <sineaddejager@gmail.com>
2025-07-21 10:02:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-# st.title("MoodiJournal📓")
-# st.write("Welcome to MoodiJournal, a safe space to write about your day or your feelings throughout the day and assess your mood from a nuetral perpective, "
-#                  " log your mood and content from your journal for you to reflect on and recieve a special message based off of your mood to either support or enhance you day.")
-# st.write("Happy Journalling!!!🌸")
-# st.subheader("Write about your day and get insights on your mood.✍🏻")
+st.title("MoodiJournal📓")
+st.write("Welcome to MoodiJournal, a safe space to write about your day or your feelings throughout the day and assess your mood from a nuetral perpective, "
+                 " log your mood and content from your journal for you to reflect on and recieve a special message based off of your mood to either support or enhance you day.")
+st.write("Happy Journalling!!!🌸")
+st.subheader("Write about your day and get insights on your mood.✍🏻")
